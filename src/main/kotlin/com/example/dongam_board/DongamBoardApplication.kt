5b983b6d4ff0a90532ea6a82303b5803a34c11ee
yu-heejin <jkgt0521@naver.com>
2023-02-03 10:44:27 +0900

    feat: project setting

---
+++ src/main/kotlin/com/example/dongam_board/DongamBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dongam_board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DongamBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<DongamBoardApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dongam_board/DongamBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dongam_board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DongamBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
