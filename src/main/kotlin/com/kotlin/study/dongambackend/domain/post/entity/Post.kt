ab2381fa62f29f840bfa16701c74dfdd009ad98e
yu-heejin <jkgt0521@naver.com>
2023-03-26 16:45:40 +0900

    feat: [#6] Post Entity 생성

---
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/entity/Post.kt
@@ -0,0 +1,39 @@
+package com.kotlin.study.dongambackend.domain.post.entity
+
+import lombok.AllArgsConstructor
+import lombok.Builder
+import lombok.Getter
+import lombok.NoArgsConstructor
+import org.hibernate.annotations.ColumnDefault
+import javax.persistence.*
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "post")
+@Entity
+open class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private var id: Long? = null
+
+    @Column(name = "device_token", nullable = false)
+    private var deviceToken: String? = null
+
+    @Column(name = "title", nullable = false)
+    private var title: String? = null
+
+    @Column(name = "content", nullable = false)
+    private var content: String? = null
+
+    @Column(name = "likes", nullable = false)
+    @ColumnDefault("0")
+    @Builder.Default
+    private var likes: Int? = 0
+
+    @Column(name = "report_count", nullable = false)
+    @ColumnDefault("0")
+    @Builder.Default
+    private var reportCount: Int? = 0
+}
