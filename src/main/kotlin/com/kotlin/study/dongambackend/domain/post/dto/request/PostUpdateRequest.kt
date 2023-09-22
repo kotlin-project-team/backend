3dc257b372fb89aab739cedde4c80009bae13a67
yu-heejin <jkgt0521@naver.com>
2023-09-23 03:04:21 +0900

    :recycle:  refactor: data class var -> val

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostUpdateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostUpdateRequest.kt
@@ -1,6 +1,6 @@
 package com.kotlin.study.dongambackend.domain.post.dto.request
 
 data class PostUpdateRequest (
-    var title: String,
-    var content: String,
+    val title: String,
+    val content: String,
 )
