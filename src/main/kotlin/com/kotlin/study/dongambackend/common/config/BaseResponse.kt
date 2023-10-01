382bebb5ac1618993c2494db1d4ff3adbc8e0014
yubin <garmalhy1@gmail.com>
2023-10-01 18:32:42 +0900

    ♻️  refactor: comment base resp 일부 변경 [#8]
    ♻️  refactor: comment base resp 일부 변경 [#8]

--- src/main/kotlin/com/kotlin/study/dongambackend/common/config/BaseResponse.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/common/config/BaseResponse.kt
@@ -1,12 +1,29 @@
 package com.kotlin.study.dongambackend.common.config
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+@JsonPropertyOrder("code", "success", "result")
 data class BaseResponse<T>(
     val code: Int,
-    val status: String,
-
-    private var result: T? = null
+    val status: String?,
+    private val result: T? = null
 ) {
-    constructor(status: ResponseStatus, result: T?) : this(status.code, status.status) {
-        this.result = result
+
+    // success
+    constructor(status: ResponseStatus) : this(status.code, status.statusMsg, null)
+    constructor(result: T, status: ResponseStatus) : this(status.code, status.statusMsg, result)
+    // fail
+    constructor(status: ResponseStatus, isFailure: Boolean = false) : this(status.code, status.statusMsg,null)
+
+//    constructor(status: ResponseStatus) : this(status.isSuccess, status.statusMsg, status.code, null)
+
+    fun convert(): ResponseEntity<BaseResponse<T>> {
+        val headers = HttpHeaders()
+        headers["Content-Type"] = "application/json; charset=UTF-8"
+        return ResponseEntity.status(code).headers(headers).body(this)
     }
 }
+
