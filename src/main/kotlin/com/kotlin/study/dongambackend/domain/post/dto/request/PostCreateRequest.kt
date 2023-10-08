fd865c4a16b1d6e7952bb81a7448091512cfe2f2
yu-heejin <jkgt0521@naver.com>
2023-10-08 18:44:53 +0900

    :recycle:  refactor: 게시판 카테고리 validate [#5]

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
@@ -1,7 +1,9 @@
 package com.kotlin.study.dongambackend.domain.post.dto.request
 
+import com.kotlin.study.dongambackend.common.type.BoardCategoryType
+
 data class PostCreateRequest (
     val title: String,
     val content: String,
-    val category: String,
+    val category: BoardCategoryType,
 )
--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/entity/Post.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/entity/Post.kt
@@ -1,6 +1,7 @@
 package com.kotlin.study.dongambackend.domain.post.entity
 
 import com.kotlin.study.dongambackend.common.entity.BaseTimeEntity
+import com.kotlin.study.dongambackend.common.type.BoardCategoryType
 import com.kotlin.study.dongambackend.domain.post.dto.request.PostUpdateRequest
 import lombok.AllArgsConstructor
 import lombok.Builder
@@ -26,7 +27,9 @@ class Post(
 
     var content: String,
 
-    val category: String,
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    val category: BoardCategoryType,
 
     @ColumnDefault("0")
     val likes: Int? = 0,
