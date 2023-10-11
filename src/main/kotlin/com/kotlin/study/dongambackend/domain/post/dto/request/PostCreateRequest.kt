7ee1947c258615f88e492431c19fc1a2fdd9f6fb
yu-heejin <jkgt0521@naver.com>
2023-10-11 22:17:45 +0900

    :sparkles:  feat: request validate 추가 [#21]

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
@@ -1,9 +1,15 @@
 package com.kotlin.study.dongambackend.domain.post.dto.request
 
 import com.kotlin.study.dongambackend.common.type.BoardCategoryType
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
 
 data class PostCreateRequest (
+    @field:NotBlank(message = "제목 값은 필수입니다.")
     val title: String,
+
+    @field:NotEmpty(message = "내용을 입력해주세요.")
     val content: String,
+
     val category: BoardCategoryType,
 )
