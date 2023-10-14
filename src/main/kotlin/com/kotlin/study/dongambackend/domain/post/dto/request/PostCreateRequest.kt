24f71d6610414af71ea93d07cb31efdd5f993119
yu-heejin <jkgt0521@naver.com>
2023-10-15 01:14:40 +0900

    :recycle:  refactor: validate 필드 부분 nullable 타입으로 수정 [#21]

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
@@ -5,11 +5,11 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
 data class PostCreateRequest (
-    @field:NotBlank(message = "제목 값은 필수입니다.")
-    val title: String,
+    @field:NotBlank(message = "lakjdflakdjflskdjfshfksh")
+    val title: String?,
 
-    @field:NotEmpty(message = "내용을 입력해주세요.")
-    val content: String,
+    @field:NotEmpty(message = "alskdalskjd")
+    val content: String?,
 
     val category: BoardCategoryType,
 )
--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostUpdateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostUpdateRequest.kt
@@ -4,9 +4,9 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
 data class PostUpdateRequest (
-    @field:NotBlank(message = "제목 값은 필수입니다.")
-    val title: String,
+    @field:NotBlank(message = "title is nessaasdj")
+    val title: String?,
 
     @field:NotEmpty(message = "내용을 입력해주세요.")
-    val content: String,
+    val content: String?,
 )
