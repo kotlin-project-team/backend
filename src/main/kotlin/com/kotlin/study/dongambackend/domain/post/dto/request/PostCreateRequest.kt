a6b5cded597d7c2136ec0b7d6229b192d35afa38
yu-heejin <jkgt0521@naver.com>
2023-10-15 01:53:01 +0900

    :recycle:  refactor: 오류 메세지 수정 [#21]

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostCreateRequest.kt
@@ -5,10 +5,10 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
 data class PostCreateRequest (
-    @field:NotBlank(message = "lakjdflakdjflskdjfshfksh")
+    @field:NotBlank(message = "제목 값은 필수입니다.")
     val title: String?,
 
-    @field:NotEmpty(message = "alskdalskjd")
+    @field:NotEmpty(message = "내용을 입력해주세요.")
     val content: String?,
 
     val category: BoardCategoryType,
--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostUpdateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/dto/request/PostUpdateRequest.kt
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
 data class PostUpdateRequest (
-    @field:NotBlank(message = "title is nessaasdj")
+    @field:NotBlank(message = "제목 값은 필수입니다.")
     val title: String?,
 
     @field:NotEmpty(message = "내용을 입력해주세요.")
