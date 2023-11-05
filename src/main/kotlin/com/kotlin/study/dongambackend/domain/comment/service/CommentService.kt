52764b52520aadd931e758dcbf66001ea8170fd7
yu-heejin <jkgt0521@naver.com>
2023-11-05 17:53:29 +0900

    :recycle:  refactor: ResponseStatus 이름 중복 문제 수정

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/service/CommentService.kt
@@ -2,7 +2,7 @@ package com.kotlin.study.dongambackend.domain.comment.service
 
 import com.kotlin.study.dongambackend.common.exception.BaseException
 import com.kotlin.study.dongambackend.domain.comment.dto.request.CommentCreateRequest
-import com.kotlin.study.dongambackend.common.type.ResponseStatus
+import com.kotlin.study.dongambackend.common.type.ResponseStatusType
 import com.kotlin.study.dongambackend.domain.comment.dto.request.CommentReportRequest
 import com.kotlin.study.dongambackend.domain.comment.dto.request.CommentUpdateRequest
 import com.kotlin.study.dongambackend.domain.comment.entity.Comment
@@ -19,16 +19,16 @@ class CommentService(private val commentRepository: CommentRepository, private v
     fun createComment(commentCreateRequest: CommentCreateRequest): Long? {
         val comment = Comment(commentCreateRequest.content)
         // TODO: Unauthorized 처리
-        comment.postId ?: throw BaseException(ResponseStatus.NOT_FOUND)
+        comment.postId ?: throw BaseException(ResponseStatusType.NOT_FOUND)
         commentRepository.save(comment)
 
         return comment.id
     }
 
     fun updateComment(commentUpdateRequest: CommentUpdateRequest, commentId: Long): Comment {
-        val comment = commentRepository.findById(commentId).orElseGet { throw BaseException(ResponseStatus.BAD_REQUEST) }
+        val comment = commentRepository.findById(commentId).orElseGet { throw BaseException(ResponseStatusType.BAD_REQUEST) }
         // TODO: Unauthorized 및 ForbiddenToken 처리
-        comment.postId ?: throw BaseException(ResponseStatus.NOT_FOUND)
+        comment.postId ?: throw BaseException(ResponseStatusType.NOT_FOUND)
         comment.updateComment(commentUpdateRequest)
         commentRepository.save(comment)
 
@@ -36,9 +36,9 @@ class CommentService(private val commentRepository: CommentRepository, private v
     }
 
     fun deleteComment(commentId: Long) {
-        val comment = commentRepository.findById(commentId).orElseThrow { BaseException(ResponseStatus.BAD_REQUEST) }
+        val comment = commentRepository.findById(commentId).orElseThrow { BaseException(ResponseStatusType.BAD_REQUEST) }
         // TODO: Unauthorized 및 ForbiddenToken 처리
-        val commentIdToDelete = comment.id ?: throw BaseException(ResponseStatus.NOT_FOUND)
+        val commentIdToDelete = comment.id ?: throw BaseException(ResponseStatusType.NOT_FOUND)
         commentRepository.deleteById(commentIdToDelete)
     }
 
