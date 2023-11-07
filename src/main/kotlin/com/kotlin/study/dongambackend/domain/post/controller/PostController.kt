606f159a70f91f77710a9937f16a872e8cf972f0
yubin <garmalhy1@gmail.com>
2023-11-07 13:02:50 +0900

    ♻️  refactor: baseresp 에 따른 return 변경

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/controller/PostController.kt
@@ -1,7 +1,9 @@
 package com.kotlin.study.dongambackend.domain.post.controller
 
 import com.kotlin.study.dongambackend.common.annotation.ValidateCategory
+import com.kotlin.study.dongambackend.common.dto.BaseResponse
 import com.kotlin.study.dongambackend.common.type.BoardCategoryType
+import com.kotlin.study.dongambackend.common.type.ResponseStatusType
 import com.kotlin.study.dongambackend.domain.post.dto.request.PostCreateRequest
 import com.kotlin.study.dongambackend.domain.post.dto.request.PostUpdateRequest
 import com.kotlin.study.dongambackend.domain.post.dto.response.PostCategoryFreeResponse
@@ -27,9 +29,9 @@ class PostController(private val postService: PostService) {
         pageable: Pageable,
         @RequestParam(value = "category", required = true)
         @ValidateCategory(enumClass = BoardCategoryType::class) category: BoardCategoryType
-    ): ResponseEntity<List<PostCategoryFreeResponse>> {
+    ): ResponseEntity<BaseResponse<List<PostCategoryFreeResponse>>> {
         val posts = postService.getAllPost(pageable, category)
-        return ResponseEntity.ok().body(posts)
+        return BaseResponse(ResponseStatusType.SUCCESS, posts).convert()
     }
 
     @GetMapping("/{postId}")
