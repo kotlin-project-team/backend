2748a1c784fc53a062fc481efb63066675127967
yu-heejin <jkgt0521@naver.com>
2023-11-19 20:25:18 +0900

    :sparkles:  feat: User 도메인 Controller 추가 [#16]

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/user/controller/UserController.kt
@@ -1,4 +1,19 @@
 package com.kotlin.study.dongambackend.domain.user.controller
 
-class UserController {
+import com.kotlin.study.dongambackend.domain.user.dto.request.UserCreateRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(private val userService: UserService) {
+
+    @PostMapping
+    fun createUser(@Valid @RequestBody userCreateRequest: UserCreateRequest): ResponseEntity<Unit> {
+
+    }
 }
