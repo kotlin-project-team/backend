50e175a031ad1ef1a11029a54c4bcc1d0cccf3ee
yu-heejin <jkgt0521@naver.com>
2023-11-30 20:34:20 +0900

    :sparkles:  feat: SecurityConfig 추가 [#16]

--- src/main/kotlin/com/kotlin/study/dongambackend/security/config/PasswordConfig.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/security/config/PasswordConfig.kt
@@ -1,4 +1,13 @@
 package com.kotlin.study.dongambackend.security.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
 class PasswordConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
--- src/main/kotlin/com/kotlin/study/dongambackend/security/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/security/config/WebSecurityConfig.kt
@@ -1,4 +1,28 @@
 package com.kotlin.study.dongambackend.security.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
+@Configuration
 class WebSecurityConfig {
+
+    @Bean
+    fun configure(httpSecurity: HttpSecurity): DefaultSecurityFilterChain {
+        return httpSecurity
+            .csrf().disable()
+            .httpBasic().disable()
+            .formLogin().disable()
+            .authorizeHttpRequests {
+                it.requestMatchers(AntPathRequestMatcher("/api/**")).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .build()
+    }
 }
