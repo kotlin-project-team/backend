06131c41f0f55501a6ee5d919801519ac04e1bdf
yubin <garmalhy1@gmail.com>
2024-01-14 18:38:09 +0900

    💫 chore: auth 일부 추가 (#28))

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/dto/request/CommentCreateRequest.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/dto/request/CommentCreateRequest.kt
@@ -1,7 +1,12 @@
 package com.kotlin.study.dongambackend.domain.comment.dto.request
 
+import com.kotlin.study.dongambackend.domain.post.entity.Post
+import javax.validation.constraints.NotEmpty
+
 data class CommentCreateRequest (
+    val postId: Post,
+    
+    @field:NotEmpty(message = "내용을 입력해주세요.")
     var content: String,
 ) {
-
 }
--- src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/dto/response/CommentResponse.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/dto/response/CommentResponse.kt
@@ -1,8 +1,11 @@
 package com.kotlin.study.dongambackend.domain.comment.dto.response
 
+import com.kotlin.study.dongambackend.domain.post.entity.Post
+import com.kotlin.study.dongambackend.domain.user.entity.User
+
 data class CommentResponse(
     val id: Long?,
-    val userId: Long,
-    val postId: Long,
+    val userId: User,
+    val postId: Post,
     val content: String?
 )
