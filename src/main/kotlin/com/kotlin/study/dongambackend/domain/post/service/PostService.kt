faa46d9864d6302e0ab12e1c3a0be58173d9b28d
yu-heejin <jkgt0521@naver.com>
2024-01-14 17:22:33 +0900

    :recycle:  refactor: postResponse 이름 변경

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/post/service/PostService.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/post/service/PostService.kt
@@ -4,7 +4,7 @@ import com.kotlin.study.dongambackend.common.type.BoardCategoryType
 import com.kotlin.study.dongambackend.domain.post.dto.entitykey.PostLikeKey
 import com.kotlin.study.dongambackend.domain.post.dto.request.PostCreateRequest
 import com.kotlin.study.dongambackend.domain.post.dto.request.PostUpdateRequest
-import com.kotlin.study.dongambackend.domain.post.dto.response.PostCategoryFreeResponse
+import com.kotlin.study.dongambackend.domain.post.dto.response.GetAllPostByCategoryResponse
 import com.kotlin.study.dongambackend.domain.post.entity.Post
 import com.kotlin.study.dongambackend.domain.post.entity.PostLike
 import com.kotlin.study.dongambackend.domain.post.mapper.PostMapper
@@ -28,7 +28,7 @@ class PostService(
 ) {
 
     @Transactional(readOnly = true)
-    fun getAllPost(pageable: Pageable, category: BoardCategoryType): List<PostCategoryFreeResponse> {
+    fun getAllPost(pageable: Pageable, category: BoardCategoryType): List<GetAllPostByCategoryResponse> {
         return postQueryDslRepository.findAllPost(pageable, category)
     }
 
