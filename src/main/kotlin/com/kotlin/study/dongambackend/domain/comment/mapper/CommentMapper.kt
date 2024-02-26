99901028b0b47a619089e1a4e80de4b384c74c2c
devyu <80163835+yubin21@users.noreply.github.com>
2024-02-26 15:16:19 +0900

    🐛  fix: 무한스크롤 구현 문제 수정 (#51)

--- src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/mapper/CommentMapper.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/mapper/CommentMapper.kt
@@ -2,6 +2,8 @@ package com.kotlin.study.dongambackend.domain.comment.mapper
 
 import com.kotlin.study.dongambackend.domain.comment.dto.request.CommentCreateRequest
 import com.kotlin.study.dongambackend.domain.comment.dto.response.CommentResponse
+import com.kotlin.study.dongambackend.domain.comment.dto.response.FindAllCommentResponse
+import com.kotlin.study.dongambackend.domain.comment.dto.response.GetAllCommentResponse
 import com.kotlin.study.dongambackend.domain.comment.entity.Comment
 import com.kotlin.study.dongambackend.domain.post.entity.Post
 import com.kotlin.study.dongambackend.domain.user.entity.User
@@ -10,18 +12,11 @@ import org.springframework.stereotype.Component
 @Component
 class CommentMapper {
 
-    fun convertCommentsToResponses(results: List<Comment>): List<CommentResponse> {
-        return results.map { comment ->
-            CommentResponse(
-                id = comment.id,
-                userId = comment.userId,
-                postId = comment.postId,
-                content = comment.content
-            )
-        }
-    }
-
     fun convertCreateCommentReqDtoToEntity(user: User, post: Post, createRequest: CommentCreateRequest): Comment {
         return Comment(user, post, createRequest.content)
+    }
+
+    fun toGetAllCommentResponse(comments: List<FindAllCommentResponse>, postCount: Int): GetAllCommentResponse {
+        return GetAllCommentResponse(comments, postCount)
     }
 }
--- src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/repository/CommentQueryDslRepository.kt
+++ src/main/kotlin/com/kotlin/study/dongambackend/domain/comment/repository/CommentQueryDslRepository.kt
@@ -1,10 +1,9 @@
 package com.kotlin.study.dongambackend.domain.comment.repository
 
-import com.kotlin.study.dongambackend.domain.comment.dto.response.CommentResponse
-import com.kotlin.study.dongambackend.domain.comment.entity.Comment
+import com.kotlin.study.dongambackend.domain.comment.dto.response.FindAllCommentResponse
+import com.kotlin.study.dongambackend.domain.comment.dto.response.QFindAllCommentResponse
 import com.kotlin.study.dongambackend.domain.comment.entity.QComment
-import com.kotlin.study.dongambackend.domain.comment.mapper.CommentMapper
-import com.kotlin.study.dongambackend.domain.comment.service.CommentService
+import com.kotlin.study.dongambackend.domain.post.dto.response.QUserInformation
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.*
 import org.springframework.stereotype.Repository
@@ -13,14 +12,35 @@ import org.springframework.stereotype.Repository
 class CommentQueryDslRepository(val queryDslFactory: JPAQueryFactory) {
     val qComment = QComment.comment;
 
-    fun searchCommentsBySlice(lastCommentId: Long, pageable: Pageable): List<Comment> {
-        return queryDslFactory.selectFrom(qComment)
+    fun searchCommentsByPostId(postId: Long, lastCommentId: Long, pageable: Pageable): List<FindAllCommentResponse> {
+        // 기본 쿼리 생성
+        val query = queryDslFactory
+            .select(
+                QFindAllCommentResponse(
+                    qComment.id,
+                    QUserInformation(
+                        qComment.userId.id,
+                        qComment.userId.studentId,
+                        qComment.userId.nickname
+                    ),
+                    qComment.postId.id,
+                    qComment.content
+                )
+            )
+            .from(qComment)
             .where(
                 qComment.isDeleted.eq(false),
-                qComment.id.gt(lastCommentId)
+                qComment.postId.id.eq(postId)
             )
-            .orderBy(qComment.id.asc())
-            .limit((pageable.pageSize + 1).toLong())
+
+        // lastCommentId가 0보다 클 경우, 해당 조건을 추가
+        if (lastCommentId > 0) {
+            query.where(qComment.id.lt(lastCommentId))
+        }
+
+        // 최신 댓글부터 조회하고, 페이지 사이즈에 맞게 제한
+        return query.orderBy(qComment.id.desc())
+            .limit(pageable.pageSize.toLong())
             .fetch()
     }
 }
